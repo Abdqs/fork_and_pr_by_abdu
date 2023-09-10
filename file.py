23e7777faea49298f2e3762f7cc3fd24debb26cc
Jaleed <k224912@nu.edu.pk>
2023-09-10 13:08:47 +0500

    edited the file.py

--- file.py
+++ file.py
@@ -2,3 +2,4 @@ print("jaleed khan has made this file ")
 #bilal and abdullah i want  you to write something in here so i could know
 #you guys succesfully cloned my git repo and succesfully made  a  pr into my 
 #second branch  
+print("lets make a devops project")
